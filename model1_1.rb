53198e3867831ffa04cbffc7f851c37b7a68aace
Eldridge Turambi <eturambi95@gmail.com>
2021-07-27 15:53:56 -0600

    3rd commit w/ integer and float examples

--- model1_1.rb
+++ model1_1.rb
@@ -2,3 +2,11 @@
 school_name = "Turing School of Software and Design"
 program_name = "Back End Engineering Program"
 cohort_name = "2108"
+
+#Integer data type examples
+number_of_dogs = 0
+outside_temperature_fahrenheit = 90
+
+#Float data type examples
+interest_rate = 0.05
+value_of_pi = 3.14
